b9538049862e05b3f5a67bd11bc09ded6ca9f198
HaJinhyeok <hjhmoon61@naver.com>
2025-02-25 10:35:01 +0900

    2025/02/25
    Init project

---
+++ Assets/3.Scripts/DinoController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class DinoController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Move()
+    {
+
+    }
+}
